8876b16d560f1c17a9e50620664618e49adc49fe
Leif Coleman <leif.coleman@irdeto.com>
2018-06-05 11:50:53 -0700

    Update SQL

--- coss.sql
+++ coss.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2018 at 02:06 AM
+-- Generation Time: Jun 05, 2018 at 08:11 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.1.12
 
@@ -41,96 +41,71 @@ CREATE TABLE `value` (
 --
 
 INSERT INTO `value` (`id`, `name`, `symbol_cmc`, `symbol_coss`, `value`) VALUES
-(1, '', 'LALA', 'LALA', '0.0674651'),
-(2, '', 'ADI', 'ADI', '0.0175822'),
-(3, '', 'BCH', 'BCH', '1105.8'),
-(4, '', 'BLZ', 'BLZ', '0.412602'),
-(5, '', 'BNT', 'BNT', '4.10917'),
-(6, '', 'BPL', 'BPL', '0.13903'),
-(7, '', 'BTC', 'BTC', '7514.47'),
+(1, 'LALA World', 'LALA', 'LALA', NULL),
+(2, 'Aditus', 'ADI', 'ADI', NULL),
+(3, 'Bitcoin Cash', 'BCH', 'BCH', NULL),
+(4, 'Bluzelle', 'BLZ', 'BLZ', NULL),
+(5, 'Bancor', 'BNT', 'BNT', NULL),
+(6, 'Blockpool', 'BPL', 'BPL', NULL),
+(7, 'Bitcoin', 'BTC', 'BTC', NULL),
 (8, '', 'BWT', 'BWT', NULL),
-(9, '', 'CAN', 'CAN', '0.153313'),
-(10, '', 'COSS', 'COSS', NULL),
-(11, '', 'CRED-VERIFY', 'CRED-VERIFY', NULL),
-(12, '', 'CREDITS', 'CREDITS', NULL),
-(13, '', 'CVC', 'CVC', '0.308849'),
-(14, '', 'DASH', 'DASH', '317.026'),
-(15, '', 'DAT', 'DAT', '0.0240359'),
-(16, '', 'ENJ', 'ENJ', NULL),
-(17, '', 'EOS', 'EOS', NULL),
-(18, '', 'ETH', 'ETH', NULL),
-(19, '', 'FDX', 'FDX', NULL),
-(20, '', 'FRV', 'FRV', NULL),
-(21, '', 'FXT', 'FXT', NULL),
-(22, '', 'FYN', 'FYN', NULL),
-(23, '', 'GAT', 'GAT', NULL),
+(9, 'CanYaCoin', 'CAN', 'CAN', NULL),
+(10, 'COSS', 'COSS', 'COSS', NULL),
+(11, 'Verify', 'CRED', 'CRED-VERIFY', NULL),
+(12, 'Credits', 'CS', 'CREDITS', NULL),
+(13, 'Civic', 'CVC', 'CVC', NULL),
+(14, 'Dash', 'DASH', 'DASH', NULL),
+(15, 'Datum', 'DAT', 'DAT', NULL),
+(16, 'Enjin Coin', 'ENJ', 'ENJ', NULL),
+(17, 'EOS', 'EOS', 'EOS', NULL),
+(18, 'Ethereum', 'ETH', 'ETH', NULL),
+(19, 'FidentiaX', 'FDX', 'FDX', NULL),
+(20, 'Fitrova', 'FRV', 'FRV', NULL),
+(21, 'FuzeX', 'FXT', 'FXT', NULL),
+(22, 'FundYourselfNow', 'FYN', 'FYN', NULL),
+(23, 'Gatcoin', 'GAT', 'GAT', NULL),
 (24, '', 'H2O', 'H2O', NULL),
-(25, '', 'HGT', 'HGT', NULL),
-(26, '', 'ICX', 'ICX', NULL),
-(27, '', 'IND', 'IND', NULL),
-(28, '', 'ITT', 'ITT', NULL),
-(29, '', 'JET', 'JET', NULL),
-(30, '', 'KIN', 'KIN', NULL),
-(31, '', 'KNC', 'KNC', NULL),
+(25, 'HelloGold', 'HGT', 'HGT', NULL),
+(26, 'ICON', 'ICX', 'ICX', NULL),
+(27, 'Indorse Token', 'IND', 'IND', NULL),
+(28, 'Intelligent Trading Foundation', 'ITT', 'ITT', NULL),
+(29, 'Jetcoin', 'JET', 'JET', NULL),
+(30, 'Kin', 'KIN', 'KIN', NULL),
+(31, 'Kyber Network', 'KNC', 'KNC', NULL),
 (32, '', 'LA', 'LA', NULL),
-(33, '', 'ARK', 'ARK', NULL),
+(33, 'Ark', 'ARK', 'ARK', NULL),
 (34, '', 'LAN', 'LAN', NULL),
-(35, '', 'LINK', 'LINK', NULL),
-(36, '', 'LSK', 'LSK', NULL),
-(37, '', 'LTC', 'LTC', NULL),
-(38, '', 'MRK', 'MRK', NULL),
-(39, '', 'NOX', 'NOX', NULL),
-(40, '', 'OMG', 'OMG', NULL),
-(41, '', 'PAY', 'PAY', NULL),
+(35, 'ChainLink', 'LINK', 'LINK', NULL),
+(36, 'Lisk', 'LSK', 'LSK', NULL),
+(37, 'Litecoin', 'LTC', 'LTC', NULL),
+(38, 'MARK.SPACE', 'MRK', 'MRK', NULL),
+(39, 'Nitro', 'NOX', 'NOX', NULL),
+(40, 'OmiseGO', 'OMG', 'OMG', NULL),
+(41, 'TenX', 'PAY', 'PAY', NULL),
 (42, '', 'PGT', 'PGT', NULL),
-(43, '', 'PIX', 'PIX', NULL),
-(44, '', 'POE', 'POE', NULL),
-(45, '', 'PRIX', 'PRIX', NULL),
-(46, '', 'REQ', 'REQ', NULL),
-(47, '', 'SENC', 'SENC', NULL),
+(43, 'Lampix', 'PIX', 'PIX', NULL),
+(44, 'Po.et', 'POE', 'POE', NULL),
+(45, 'Privatix', 'PRIX', 'PRIX', NULL),
+(46, 'Request Network', 'REQ', 'REQ', NULL),
+(47, 'Sentinel Chain', 'SENC', 'SENC', NULL),
 (48, '', 'SMDX', 'SMDX', NULL),
-(49, '', 'SNM', 'SNM', NULL),
-(50, '', 'STX', 'STX', NULL),
-(51, '', 'SUB', 'SUB', NULL),
+(49, 'SONM', 'SNM', 'SNM', NULL),
+(50, 'Stox', 'STX', 'STX', NULL),
+(51, 'Substratum', 'SUB', 'SUB', NULL),
 (52, '', 'TEU', 'TEU', NULL),
-(53, '', 'TIG', 'TIG', NULL),
-(54, '', 'TRAK', 'TRAK', NULL),
-(55, '', 'UFR', 'UFR', NULL),
-(56, '', 'USD', 'USD', NULL),
-(57, '', 'VEN', 'VEN', NULL),
-(58, '', 'VZT', 'VZT', NULL),
-(59, '', 'WAVES', 'WAVES', NULL),
-(60, '', 'WISH', 'WISH', NULL),
-(61, '', 'WTC', 'WTC', NULL),
-(62, '', 'XDCE', 'XDCE', NULL),
-(63, '', 'XEM', 'XEM', NULL),
-(64, '', 'XNK', 'XNK', NULL),
-(65, '', 'ZEN', 'ZEN', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `volume`
---
-
-CREATE TABLE `volume` (
-  `id` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `value` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `volume`
---
-
-INSERT INTO `volume` (`id`, `date`, `value`) VALUES
-(2, '2018-04-24 13:00:00', 1081234),
-(9, '2018-04-24 13:44:25', 1081234),
-(11, '2018-04-24 13:44:30', 1081234),
-(13, '2018-04-24 13:44:35', 1081234),
-(15, '2018-04-24 13:44:40', 1081234),
-(17, '2018-04-24 13:44:45', 1081234),
-(19, '2018-04-24 13:44:50', 1081234);
+(53, 'Tigereum', 'TIG', 'TIG', NULL),
+(54, 'TrakInvest', 'TRAK', 'TRAK', NULL),
+(55, 'Upfiring', 'UFR', 'UFR', NULL),
+(56, 'United States Dollar', 'USD', 'USD', NULL),
+(57, 'VeChain', 'VEN', 'VEN', NULL),
+(58, 'Vezt', 'VZT', 'VZT', NULL),
+(59, 'Waves', 'WAVES', 'WAVES', NULL),
+(60, 'MyWish', 'WISH', 'WISH', NULL),
+(61, 'Waltonchain', 'WTC', 'WTC', NULL),
+(62, 'XinFin Network', 'XDCE', 'XDCE', NULL),
+(63, 'NEM', 'XEM', 'XEM', NULL),
+(64, 'Ink Protocol', 'XNK', 'XNK', NULL),
+(65, 'ZenCash', 'ZEN', 'ZEN', NULL);
 
 --
 -- Indexes for dumped tables
@@ -143,13 +118,6 @@ ALTER TABLE `value`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `volume`
---
-ALTER TABLE `volume`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `date` (`date`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -158,12 +126,6 @@ ALTER TABLE `volume`
 --
 ALTER TABLE `value`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
-
---
--- AUTO_INCREMENT for table `volume`
---
-ALTER TABLE `volume`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
