8ed3a5f86fce2636e933fcbe015064b6490525cc
Tundra Fizz <coleman.leif@gmail.com>
2018-08-17 14:53:11 -0700

    Update log.js

--- node/log.js
+++ node/log.js
@@ -3,9 +3,12 @@ var moment = require("moment");
 
 function Log(){}
 
-Log.prototype.Write = function(msg){
+Log.prototype.Write = function(msg, consoleLog = false){
   var now = moment().format("MMM DD, YYYY @ HH:mm:ss");
   msg = `${now}: ${msg}\n`;
+
+  if(consoleLog)
+    console.log(msg);
 
   fs.appendFile("log/log-coss-stats.txt", msg, function(err){
     if(err)
